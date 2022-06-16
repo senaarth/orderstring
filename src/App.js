cfe58e99dd533eafaeaf14a5b2451746879bdb55
Arthur Sena <senaarth@gmail.com>
2022-06-16 17:12:52 -0300

    multiline update

--- src/App.js
+++ src/App.js
@@ -5,18 +5,27 @@ import "./global.css";
 function App() {
     const [value, setValue] = React.useState("");
     const result = React.useMemo(() => {
-        const returnValue = value
-            .split(" ")
-            .sort((a, b) => Number(a) - Number(b))
-            .reduce((prev, curr) => {
-                if (prev.find((item) => Number(item) === Number(curr))) {
-                    return prev;
-                }
+        let lines = value.split("\n");
 
-                return [...prev, curr];
-            }, []);
+        const result = lines.reduce((prevLine, currLine) => {
+            const orderedLine = currLine
+                .split(" ")
+                .sort((a, b) => Number(a) - Number(b))
+                .reduce((prev, curr) => {
+                    if (
+                        prev.find((item) => Number(item) === Number(curr)) ||
+                        Number(curr) < 0
+                    ) {
+                        return prev;
+                    }
 
-        return returnValue.length > 1 ? returnValue : [];
+                    return [...prev, Number(curr)];
+                }, []);
+
+            return [...prevLine, orderedLine];
+        }, []);
+
+        return result;
     }, [value]);
 
     return (
@@ -26,7 +35,7 @@ function App() {
                 Copia os caracteres que o senhor quer ordenar e cola dentro
                 desse input de texto ai embaixo.
             </p>
-            <input
+            <textarea
                 value={value}
                 onChange={({ target }) => setValue(target.value)}
             />
@@ -37,9 +46,18 @@ function App() {
             <p>
                 {!!result.length && (
                     <>
-                        {result?.map((item) => {
-                            if (!Boolean(Number(item))) return null;
-                            return item < 10 ? `0${item} ` : `${item} `;
+                        {result?.map((line) => {
+                            return (
+                                <>
+                                    {line.map((item) => {
+                                        if (!Boolean(Number(item))) return null;
+                                        return item < 10
+                                            ? `0${item} `
+                                            : `${item} `;
+                                    })}
+                                    <br />
+                                </>
+                            );
                         })}
                     </>
                 )}
